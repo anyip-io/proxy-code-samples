ee99cc64f117d67eb29b5e206dabb364e32fa87a
Jonathan Dizdarevic <dizzda@gmail.com>
2023-06-07 14:35:14 +0700

    chore(ureq): improve the retry logic

--- 02-rust-ureq/src/main.rs
+++ 02-rust-ureq/src/main.rs
@@ -1,16 +1,17 @@
+use rayon::prelude::*;
+use select::document::Document;
+use select::predicate::{Class, Name, Predicate};
 use std::env;
 use std::error::Error;
 use std::io::ErrorKind;
 use std::time::{Duration, Instant};
-use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
-use rayon::prelude::*;
 use ureq::Agent;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let keyword = "boxing gloves";
     let url = format!("https://www.walmart.com/search?q={keyword}");
-    let proxy_endpoint = env::var("PROXY_ENDPOINT").expect("`PROXY_ENDPOINT` env var should be specified");
+    let proxy_endpoint =
+        env::var("PROXY_ENDPOINT").expect("`PROXY_ENDPOINT` env var should be specified");
     println!("Target URL: {url}");
 
     let proxy = ureq::Proxy::new(proxy_endpoint)?;
@@ -18,7 +19,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .proxy(proxy)
         .max_idle_connections(0)
         .max_idle_connections_per_host(0)
-        .timeout(Duration::from_secs(10))
+        .timeout(Duration::from_secs(30))
         .build();
 
     // This is call behind a proxy
@@ -29,18 +30,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut links = scrape_search_result(&resp);
     println!("List of links: {links:?}");
+    println!("----");
 
-    let mut retries = 0;
+    let mut attempt = 0;
 
-    while retries < 3 {
+    while attempt < 3 {
+        println!("Attempt #{} - scrape the list of products", attempt + 1);
+
         let products = scrape_products_par(agent.clone(), links.clone());
-        println!("success: {}/{}", products.iter()
-            .filter_map(|(url, res)| res.as_ref().ok())
-            .count(), products.len()
+
+        println!(
+            "success: {}/{}",
+            products
+                .iter()
+                .filter_map(|(url, res)| res.as_ref().ok())
+                .count(),
+            products.len()
         );
 
         println!("Error details:");
-        links = products.iter()
+        products
+            .iter()
+            .filter_map(|(_url, res)| res.as_ref().err())
+            .for_each(|e| println!("  - {}", e));
+
+        // we collect the links that have thrown an error, then overwrite the initial Vec
+        links = products
+            .iter()
             .filter(|(x, res)| res.is_err())
             .map(|(url, res)| url.to_owned())
             .collect::<Vec<_>>();
@@ -48,11 +64,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         if links.len() == 0 {
             // if there's no link's left, we have scraped the entire list of products
             break;
+        } else {
+            println!("---We'll retry the failed requests on the next round...");
+            attempt += 1;
         }
-
-        retries += 1;
     }
-
 
     Ok(())
 }
@@ -67,7 +83,11 @@ fn scrape_search_result(resp: &str) -> Vec<String> {
         for link in node.find(Name("a").and(Class("absolute"))) {
             cpt += 1;
 
-            println!("#{cpt} {} ({:?})\n", link.text(), link.attr("href").unwrap());
+            println!(
+                "#{cpt} {} ({:?})\n",
+                link.text(),
+                link.attr("href").unwrap()
+            );
             let href = link.attr("href").unwrap();
 
             if href.contains("walmart.com/") {
@@ -83,13 +103,19 @@ fn scrape_search_result(resp: &str) -> Vec<String> {
 }
 
 // parallel
-fn scrape_products_par(agent: Agent, list: Vec<String>) -> Vec<(String, Result<String, std::io::Error>)> {
-    let responses = list.par_iter().map(|url| (url.clone(), agent.get(url).call())).collect::<Vec<_>>();
+fn scrape_products_par(
+    agent: Agent,
+    list: Vec<String>,
+) -> Vec<(String, Result<String, std::io::Error>)> {
+    let responses = list
+        .par_iter()
+        .map(|url| (url.clone(), agent.get(url).call()))
+        .collect::<Vec<_>>();
     let mut contents = vec![];
 
     for (url, response) in responses {
         let res = response
-            .map_err(|e| std::io::Error::new(ErrorKind::Other,e))
+            .map_err(|e| std::io::Error::new(ErrorKind::Other, e))
             .and_then(|r| r.into_string());
 
         contents.push((url, res));
